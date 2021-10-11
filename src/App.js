335e61bd12f70694828a257feb84ccc9097d2fee
Nithun <Nithun>
2021-10-11 12:55:08 +0400

    .

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
               <ul>
                 <li>Link 1</li>
                 <li>Link 2</li>
-                <li>Link 1</li>
+                <li>Link 3</li>
+                <li>Link 3</li>
               </ul>
             </div>
             </div>
