23e8215c11f110e6ae709c9ffc7d9c95900b6cf1
apple <apple@apples-MacBook-Air.local>
2021-10-11 12:41:10 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          <div className="container">
+            <div className="logo">LOGO</div>
+            <div className="navigation-section">
+              <ul>
+                <li>Link 1</li>
+                <li>Link 1</li>
+                <li>Link 1</li>
+              </ul>
+            </div>
+            </div>
+      </header>
+
+      <wrapper>
+          <div className="container">
+          Content Section
+          </div>
+      </wrapper>
+
+      <footer>
+        <div className="conatiner">
+          Footer Content
+        </div>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
